c20c6e64d2162022f0a8564642d06ed5f33a831c
Kay <kaicipher966@gmail.com>
2023-06-05 16:10:01 -0400

    removed dist folder, updated gitignore

--- dist/middleware/not-found.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function notFoundMiddleware(req, res) {
-    res.status(404).json({ msg: 'Route does not exist' });
-}
-exports.default = notFoundMiddleware;
--- dist/routers/productRoutes.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express = require('express');
-const productRouter = express.Router();
-exports.default = productRouter;
